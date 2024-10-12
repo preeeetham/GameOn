08e63ab44b83307fa932f10ec5a64ad87e75b4ea
preeetham3113 <spreethamkumar5@gmail.com>
2024-10-12 22:10:28 +0530

    comoleted backend integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
 import Header from './components/Header.jsx';
 import Sidebar from './components/Sidebar.jsx';
 import GameList from './components/GameList.jsx';
 
 export default function App() {
   return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
-      <div className="min-h-screen bg-[#151515] text-white">
-        <Header />
-        <main className="container mx-auto px-4 py-8 flex">
-          <Sidebar />
-          <div className="flex-1 ml-8">
-            <h1 className="text-6xl font-bold mb-2">New and trending</h1>
-            <p className='mb-8'>Based on player counts and release date</p>
-            <GameList />
-          </div>
-        </main>
-      </div>
-    </Auth0Provider>
+    <div className="min-h-screen bg-[#151515] text-white">
+      <Header />
+      <main className="container mx-auto px-4 py-8 flex">
+        <Sidebar />
+        <div className="flex-1 ml-8">
+          <h1 className="text-6xl font-bold mb-2">New and trending</h1>
+          <p className='mb-8'>Based on player counts and release date</p>
+          <GameList />
+        </div>
+      </main>
+    </div>
   );
 }
