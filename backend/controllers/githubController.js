177d2213ccb1e96484f3fe81034d53381b618efe
preeetham3113 <spreethamkumar5@gmail.com>
2024-10-16 18:24:39 +0530

    minor fixes

--- backend/controllers/githubController.js
+++ backend/controllers/githubController.js
@@ -4,7 +4,7 @@ const githubAuth = passport.authenticate('github', { scope: ['user:email'] });
 
 const githubCallback = (req, res) => {
   passport.authenticate('github', { failureRedirect: '/login' })(req, res, () => {
-    res.redirect('http://localhost:5173/dashboard');
+    res.redirect('http://localhost:5173/');
   });
 };
 
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const passport = require('passport');
-const router = express.Router();
+const authRouter= express.Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', 
+authRouter.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
     res.redirect(process.env.CLIENT_URL + '/dashboard');
   }
 );
 
-router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+authRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
-router.get('/github/callback', 
+
+authRouter.get('/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   (req, res) => {
     res.redirect(process.env.CLIENT_URL + '/dashboard');
   }
 );
 
-module.exports = router;
+module.exports = authRouter;
