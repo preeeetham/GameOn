93d60182bdb24b317a95bf9fa54f98d3548a1be5
preeetham3113 <spreethamkumar5@gmail.com>
2024-10-16 18:36:57 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
