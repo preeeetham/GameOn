0c0ecbabf0160073ba2593aff08da6feef614fa5
preeetham3113 <spreethamkumar5@gmail.com>
2024-11-20 14:36:26 +0530

    changed defaults settings

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -41,7 +41,9 @@ const Dashboard = ({ user, setUser }) => {
           params.genres = 'adventure'; // Filter by genre
           break;
         default:
-          params.ordering = '-rating';
+          const today = new Date();
+          const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+          params.dates = `${lastMonth.toISOString().split('T')[0]},${today.toISOString().split('T')[0]}`;
       }
 
       const response = await axios.get('https://game-on-web.vercel.app/api/games', { params });
